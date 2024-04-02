6dcfe40cb6e8078c5db7bade1930e4cadeb405cf
mariancap <mareancap11@gmail.com>
2024-04-02 19:39:55 +0000

    clear code

--- terraform_IG.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_internet_gateway" "IG_terraform"{
-        vpc_id = aws_vpc.vpc_licenta_marian.id
-        tags = {
-         "Name" = "IG_terraform"
-}
-
-
-}
-
--- terraform_route_table_associacion.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_route_table_association" "terraform_rt_associacion"{
-        subnet_id = aws_subnet.public_subnet.id
-        route_table_id = aws_route_table.terraform_route_table.id
-
-}
-
