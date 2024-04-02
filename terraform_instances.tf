ed67b86f7c7d3e76e038df2bdb58c6edbd557b35
mariancap <mareancap11@gmail.com>
2024-04-02 16:01:11 +0000

    terraform errors resolved

--- terraform_instances.tf
+++ terraform_instances.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "marian_instance1"{
         ami= "ami-0c101f26f147fa7fd"
         instance_type = "t2.micro"
         subnet_id = aws_subnet.public_subnet.id
-        vpc_security_group_ids = [aws_security_group.terraform_security_group.id]
+        vpc_security_group_ids = [aws_security_group.sg_terraform.id]
         key_name = "DevOpsKey_Licenta"
 
         tags = {
@@ -17,7 +17,7 @@ resource "aws_instance" "marian_instance2"{
         ami= "ami-0c101f26f147fa7fd"
         instance_type = "t2.micro"
         subnet_id = aws_subnet.public_subnet.id
-        vpc_security_group_ids = [aws_security_group.terraform_security_group.id]
+        vpc_security_group_ids = [aws_security_group.sg_terraform.id]
         key_name = "DevOpsKey_Licenta"
 
         tags = {
@@ -30,7 +30,7 @@ resource "aws_instance" "marian_instance3"{
         ami= "ami-0c101f26f147fa7fd"
         instance_type = "t2.micro"
         subnet_id = aws_subnet.public_subnet.id
-        vpc_security_group_ids = [aws_security_group.terraform_security_group.id]
+        vpc_security_group_ids = [aws_security_group.sg_terraform.id]
         key_name = "DevOpsKey_Licenta"
 
         tags = {
