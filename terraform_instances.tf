e958a0338c92cddbf26f4ac9a6aa76ed2cd3707c
mariancap <mareancap11@gmail.com>
2024-04-02 12:17:06 +0000

    Terraform files to push

---
+++ terraform_instances.tf
@@ -0,0 +1,40 @@
+resource "aws_instance" "marian_instance1"{
+        ami= "ami-0c101f26f147fa7fd"
+        instance_type = "t2.micro"
+        subnet_id = aws_subnet.public_subnet.id
+        vpc_security_group_ids = [aws_security_group.terraform_security_group.id]
+        key_name = "DevOpsKey_Licenta"
+
+        tags = {
+          "Name" = "marian_instance1"
+}
+
+}
+
+
+
+resource "aws_instance" "marian_instance2"{
+        ami= "ami-0c101f26f147fa7fd"
+        instance_type = "t2.micro"
+        subnet_id = aws_subnet.public_subnet.id
+        vpc_security_group_ids = [aws_security_group.terraform_security_group.id]
+        key_name = "DevOpsKey_Licenta"
+
+        tags = {
+          "Name" = "marian_instance2"
+}
+
+}
+
+resource "aws_instance" "marian_instance3"{
+        ami= "ami-0c101f26f147fa7fd"
+        instance_type = "t2.micro"
+        subnet_id = aws_subnet.public_subnet.id
+        vpc_security_group_ids = [aws_security_group.terraform_security_group.id]
+        key_name = "DevOpsKey_Licenta"
+
+        tags = {
+          "Name" = "marian_instance3"
+}
+
+}
---
+++ terraform_vpc.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+        region = "us-east-1"
+}
+
+
+resource "aws_vpc" "vpc_licenta_marian" {
+        cidr_block = "10.0.0.0/16"
+        instance_tenancy = "default"
+
+        tags = {
+        "Name"= "vpc_licenta_marian"
+}
+
+}
+
