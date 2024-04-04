525977bb3834ecaef666276a831a79d7b3f01d7f
mariancap <130487384+mariancap@users.noreply.github.com>
2024-04-04 15:29:22 +0300

    Update ec2test.tf

--- ec2test.tf
+++ ec2test.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "myec2test" {
   ami           = "ami-0c101f26f147fa7fd"  
   instance_type = "t2.micro"
   tags = {
-    "Name" : "JenkinsInstance"
+    "Name" : "JenkinsInstance1"
   }
 }
