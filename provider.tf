c3041678a0036d7867299cb6e9a8cdf8a9ab4884
mariancap <mareancap11@gmail.com>
2024-04-04 14:19:13 +0000

    One job worked

--- provider.tf
+++ provider.tf
@@ -1,10 +1,13 @@
 terraform {
- required_providers {
-   aws = {
-     source  = "hashicorp/aws"
-     version = "~> 4.18.0"
-   }
- }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
- 
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
 }
