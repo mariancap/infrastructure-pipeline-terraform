0805737918ad1f7adb392159e599a994d5601555
mariancap <mareancap11@gmail.com>
2024-04-05 17:17:11 +0000

    Updated all code with infrastructure

--- main.tf
+++ main.tf
@@ -1,9 +1,120 @@
+resource "aws_vpc" "vpc_marian_licenta"{
+ cidr_block = var.vpc_cidr
+
+tags = {
+   Name = var.vpc_name
+ }
+
+}
+
+resource "aws_subnet" "public_subnet" {
+
+vpc_id = aws_vpc.vpc_marian_licenta.id
+cidr_block = var.public_subnet_cidr
+availability_zone = var.available_zones_us
+map_public_ip_on_launch = true
+
+tags = {
+   Name = "public_subnet_marian"
+ }
+
+}
+
+
+
+
+resource "aws_subnet" "private_subnet" {
+
+vpc_id = aws_vpc.vpc_marian_licenta.id
+cidr_block = var.private_subnet_cidr
+availability_zone = var.available_zones_us
+map_public_ip_on_launch = true
+
+tags = {
+   Name = "private_subnet_marian"
+ }
+
+}
+
+
+
+
+
+resource "aws_security_group" "terra_script_sg" {
+
+vpc_id = aws_vpc.vpc_marian_licenta.id
+
+egress {
+  from_port = 0
+  to_port = 0
+  protocol = -1
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+ingress {
+from_port = 22
+to_port = 22
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+
+}
+tags = {
+   Name = "terra_script_sg"
+   Description = "terra_script_sg"
+ }
+
+}
+
+
+resource "aws_internet_gateway" "ig_custom"{
+vpc_id = aws_vpc.vpc_marian_licenta.id
+
+tags = {
+  Name = "ig_custom"
+}
+
+}
+
+
+
+resource "aws_route_table" "rt_custom_internet"{
+
+vpc_id = aws_vpc.vpc_marian_licenta.id
+
+route{
+ cidr_block = "0.0.0.0/0"
+ gateway_id = aws_internet_gateway.ig_custom.id
+
+}
+
+route{
+  ipv6_cidr_block = "::/0"
+  gateway_id = aws_internet_gateway.ig_custom.id
+}
+
+
+tags = {
+  Name = "rt_custom_internet"
+}
+
+}
+
+resource "aws_route_table_association" "rt_association_marian"{
+   subnet_id = aws_subnet.public_subnet.id
+   route_table_id = aws_route_table.rt_custom_internet.id
+}
+
 resource "aws_instance" "my_vm" {
  ami           = var.ami //Ubuntu AMI
  instance_type = var.instance_type
+ key_name = "DevOpsKey_Licenta"
+ 
+ subnet_id = aws_subnet.public_subnet.id
+ vpc_security_group_ids = [aws_security_group.terra_script_sg.id]
+ associate_public_ip_address = true
 
  tags = {
-   Name = var.name_tag,
+   Name = var.name_tag
  }
 }
 
--- variables.tf
+++ variables.tf
@@ -1,7 +1,14 @@
 variable "ami" {
    type        = string
-   description = "Ubuntu AMI ID in N. Virginia Region"
+   description = "AMI for Ubuntu"
    default     = "ami-080e1f13689e07408"
+}
+
+variable "available_zones_us" {
+  type        = string
+  description = "Availability Zones"
+  default     = "us-east-1a"
+
 }
 
 variable "instance_type" {
@@ -12,6 +19,31 @@ variable "instance_type" {
 
 variable "name_tag" {
    type        = string
-   description = "Name of the EC2 instance"
+   description = "Instance name"
    default     = "My EC2 Instance"
 }
+
+variable "vpc_name" {
+   type        = string
+   description = "Instance VPC name"
+   default     = "Marian-us-east-1-vpc-licenta"
+}
+
+variable "vpc_cidr" {
+   type        = string
+   description = "CIDR values"
+   default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+   type        = string
+   description = "Public subnet CIDR values"
+   default     = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+   type        = string
+   description = "Private subnet CIDR values"
+   default     = "10.0.2.0/24"
+}
+
