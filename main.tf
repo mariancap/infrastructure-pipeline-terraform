34edd48d5641e2c4960d9d13f2bb4b94cb8f6467
mariancap <mareancap11@gmail.com>
2024-04-06 18:06:21 +0000

    Ansible and Tomcat install

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
-resource "aws_vpc" "vpc_marian_licenta"{
- cidr_block = var.vpc_cidr
+resource "aws_vpc" "vpc_marian_licenta" {
+  cidr_block = var.vpc_cidr
 
-tags = {
-   Name = var.vpc_name
- }
+  tags = {
+    Name = var.vpc_name
+  }
 
 }
 
 resource "aws_subnet" "public_subnet" {
 
-vpc_id = aws_vpc.vpc_marian_licenta.id
-cidr_block = var.public_subnet_cidr
-availability_zone = var.available_zones_us
-map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.vpc_marian_licenta.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = var.available_zones_us
+  map_public_ip_on_launch = true
 
-tags = {
-   Name = "public_subnet_marian"
- }
+  tags = {
+    Name = "public_subnet_marian"
+  }
 
 }
 
@@ -25,14 +25,14 @@ tags = {
 
 resource "aws_subnet" "private_subnet" {
 
-vpc_id = aws_vpc.vpc_marian_licenta.id
-cidr_block = var.private_subnet_cidr
-availability_zone = var.available_zones_us
-map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.vpc_marian_licenta.id
+  cidr_block              = var.private_subnet_cidr
+  availability_zone       = var.available_zones_us
+  map_public_ip_on_launch = true
 
-tags = {
-   Name = "private_subnet_marian"
- }
+  tags = {
+    Name = "private_subnet_marian"
+  }
 
 }
 
@@ -42,79 +42,102 @@ tags = {
 
 resource "aws_security_group" "terra_script_sg" {
 
-vpc_id = aws_vpc.vpc_marian_licenta.id
+  vpc_id = aws_vpc.vpc_marian_licenta.id
 
-egress {
-  from_port = 0
-  to_port = 0
-  protocol = -1
-  cidr_blocks = ["0.0.0.0/0"]
-}
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-ingress {
-from_port = 22
-to_port = 22
-protocol = "tcp"
-cidr_blocks = ["0.0.0.0/0"]
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
 
-}
-tags = {
-   Name = "terra_script_sg"
-   Description = "terra_script_sg"
- }
-
-}
-
-
-resource "aws_internet_gateway" "ig_custom"{
-vpc_id = aws_vpc.vpc_marian_licenta.id
-
-tags = {
-  Name = "ig_custom"
-}
+  }
+  tags = {
+    Name        = "terra_script_sg"
+    Description = "terra_script_sg"
+  }
 
 }
 
 
+resource "aws_internet_gateway" "ig_custom" {
+  vpc_id = aws_vpc.vpc_marian_licenta.id
 
-resource "aws_route_table" "rt_custom_internet"{
-
-vpc_id = aws_vpc.vpc_marian_licenta.id
-
-route{
- cidr_block = "0.0.0.0/0"
- gateway_id = aws_internet_gateway.ig_custom.id
+  tags = {
+    Name = "ig_custom"
+  }
 
 }
 
-route{
-  ipv6_cidr_block = "::/0"
-  gateway_id = aws_internet_gateway.ig_custom.id
+
+
+resource "aws_route_table" "rt_custom_internet" {
+
+  vpc_id = aws_vpc.vpc_marian_licenta.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ig_custom.id
+
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.ig_custom.id
+  }
+
+
+  tags = {
+    Name = "rt_custom_internet"
+  }
+
 }
 
-
-tags = {
-  Name = "rt_custom_internet"
-}
-
-}
-
-resource "aws_route_table_association" "rt_association_marian"{
-   subnet_id = aws_subnet.public_subnet.id
-   route_table_id = aws_route_table.rt_custom_internet.id
+resource "aws_route_table_association" "rt_association_marian" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.rt_custom_internet.id
 }
 
 resource "aws_instance" "my_vm" {
- ami           = var.ami //Ubuntu AMI
- instance_type = var.instance_type
- key_name = "DevOpsKey_Licenta"
- 
- subnet_id = aws_subnet.public_subnet.id
- vpc_security_group_ids = [aws_security_group.terra_script_sg.id]
- associate_public_ip_address = true
+  ami           = var.ami //Linux AMI
+  instance_type = var.instance_type
+  key_name      = "DevOpsKey_Licenta"
 
- tags = {
-   Name = var.name_tag
- }
+  subnet_id                   = aws_subnet.public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.terra_script_sg.id]
+  associate_public_ip_address = true
+
+
+  provisioner "remote-exec" {
+    	inline = [
+	  "sudo su -",
+	  "yum update -y",
+	  "apt install default-jdk",
+	  "cd /opt",
+	  "wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.86/bin/apache-tomcat-9.0.86.tar.gz",
+	  "tar -zvxf apache-tomcat-9.0.86.tar.gz",
+	  "cd apache-tomcat-9.0.86",
+	  "cd bin",
+	  "chmod +x startup.sh",
+	  "chmod +x shutdown.sh",
+	  "./startup.sh",
+	  "cd",
+	  "wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm",
+	  "sudo  yum install epel-release-latest-7.noarch.rpm -y",
+	  "yum update -y",
+	  "yum install git python python-devel python-pip openssl ansible -y",
+	  "ansible -version",
+]
+
+}
+  tags = {
+    Name = var.name_tag
+  }
 }
 
--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "ami" {
-   type        = string
-   description = "AMI for Ubuntu"
-   default     = "ami-080e1f13689e07408"
+  type        = string
+  description = "AMI for Ubuntu"
+  default     = "ami-080e1f13689e07408"
 }
 
 variable "available_zones_us" {
@@ -12,38 +12,38 @@ variable "available_zones_us" {
 }
 
 variable "instance_type" {
-   type        = string
-   description = "Instance type"
-   default     = "t2.micro"
+  type        = string
+  description = "Instance type"
+  default     = "t2.micro"
 }
 
 variable "name_tag" {
-   type        = string
-   description = "Instance name"
-   default     = "My EC2 Instance"
+  type        = string
+  description = "Instance name"
+  default     = "My EC2 Instance"
 }
 
 variable "vpc_name" {
-   type        = string
-   description = "Instance VPC name"
-   default     = "Marian-us-east-1-vpc-licenta"
+  type        = string
+  description = "Instance VPC name"
+  default     = "Marian-us-east-1-vpc-licenta"
 }
 
 variable "vpc_cidr" {
-   type        = string
-   description = "CIDR values"
-   default     = "10.0.0.0/16"
+  type        = string
+  description = "CIDR values"
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidr" {
-   type        = string
-   description = "Public subnet CIDR values"
-   default     = "10.0.1.0/24"
+  type        = string
+  description = "Public subnet CIDR values"
+  default     = "10.0.1.0/24"
 }
 
 variable "private_subnet_cidr" {
-   type        = string
-   description = "Private subnet CIDR values"
-   default     = "10.0.2.0/24"
+  type        = string
+  description = "Private subnet CIDR values"
+  default     = "10.0.2.0/24"
 }
 
