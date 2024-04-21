6724bf6a150382908650fb1a9ecbb02b72924cbd
mariancap <mareancap11@gmail.com>
2024-04-21 15:00:42 +0000

    update key name

--- main.tf
+++ main.tf
@@ -119,17 +119,16 @@ resource "aws_key_pair" "TF_key"{
 	public_key = tls_private_key.rsa.public_key_openssh
 
 }
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
 
 resource "local_file" "TF_key" {
   content  = tls_private_key.rsa.private_key_pem
   filename = "tfkey"
 }
-
-
-resource "tls_private_key" "rsa" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}	
+	
 
 
 
